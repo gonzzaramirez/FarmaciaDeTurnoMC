5746ccc7e9855cbfb0a680ddd9402857033b7ba0
gonzzaramirez <gonza37754@gmail.com>
2025-12-29 17:25:36 -0300

    Fix SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,27 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  output: 'server',
+  integrations: [react(), sitemap()],
   site: 'https://farmaciasdeturnomc.vercel.app',
   trailingSlash: 'never',
+
   build: {
     inlineStylesheets: 'auto',
   },
+
   vite: {
     plugins: [tailwindcss()],
     ssr: {
       noExternal: ['@radix-ui/*'],
     },
   },
+
+  adapter: vercel(),
 });
