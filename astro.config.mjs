c074bb0a5ad87c1580742f6598a50c588e41a3b7
gonzzaramirez <gonza37754@gmail.com>
2025-12-29 17:41:32 -0300

    Fix robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,29 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap({
+      // AGREGA ESTO: Forzar la inclusión de tu home
+      customPages: ['https://farmaciadeturnomc.vercel.app/'],
+    }),
+  ],
   site: 'https://farmaciadeturnomc.vercel.app',
   trailingSlash: 'never',
-
   build: {
     inlineStylesheets: 'auto',
   },
-
   vite: {
     plugins: [tailwindcss()],
     ssr: {
       noExternal: ['@radix-ui/*'],
     },
   },
-
   adapter: vercel(),
 });
