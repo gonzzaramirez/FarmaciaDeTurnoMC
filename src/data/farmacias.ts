82ba584e638a90f72572eeed0011be2b1027ab3e
Gonzalo Ramirez <131042201+gonzzaramirez@users.noreply.github.com>
2026-01-31 19:15:13 -0300

    Fix turnos febrero

--- src/data/farmacias.ts
+++ src/data/farmacias.ts
@@ -1,867 +1,120 @@
 export const turnosFarmacias = [
   {
-    fecha: "2025-07-01",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-07-02",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-07-03",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-07-04",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-07-05",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-07-06",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-07-07",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-07-08",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-07-09",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-07-10",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-07-11",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-07-12",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-07-13",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-07-14",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-07-15",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-07-16",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-07-17",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-07-18",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-07-19",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-07-20",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-07-21",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-07-22",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-07-23",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-07-24",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-07-25",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-07-26",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-07-27",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-07-28",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-07-29",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-07-30",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-07-31",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-08-01",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-08-02",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-08-03",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-08-04",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-08-05",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-08-06",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-08-07",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-08-08",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-08-09",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-08-10",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-08-11",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-08-12",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-08-13",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-08-14",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-08-15",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-08-16",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-08-17",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-08-18",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-08-19",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-08-20",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-08-21",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-08-22",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-08-23",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-08-24",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-08-25",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-08-26",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-08-27",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-08-28",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-08-29",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-08-30",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-08-31",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-09-01",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-09-02",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-09-03",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-09-04",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-09-05",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-09-06",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-09-07",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-09-08",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-09-09",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-09-10",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-09-11",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-09-12",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-09-13",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-09-14",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-09-15",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-09-16",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-09-17",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-09-18",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-09-19",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-09-20",
-    farmacia: "4 SOLES II",
-  },
-  {
-    fecha: "2025-09-21",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-09-22",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-09-23",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-09-24",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-09-25",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-09-26",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-09-27",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-09-28",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-09-29",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-09-30",
-    farmacia: "DEL ESTE",
-  },
-
-  {
-    fecha: "2025-10-01",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-10-02",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-10-03",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-10-04",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-10-05",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-10-06",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-10-07",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-10-08",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-10-09",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-10-10",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-10-11",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-10-12",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-10-13",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-10-14",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-10-15",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-10-16",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-10-17",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-10-18",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-10-19",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-10-20",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-10-21",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-10-22",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-10-23",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-10-24",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-10-25",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-10-26",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-10-27",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-10-28",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-10-29",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-10-30",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-10-31",
-    farmacia: "4 SOLES",
-  },
-  // NOVIEMBRE 2025
-  {
-    fecha: "2025-11-01",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-11-02",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-11-03",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-11-04",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-11-05",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-11-06",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-11-07",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-11-08",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-11-09",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-11-10",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-11-11",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-11-12",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-11-13",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-11-14",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-11-15",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-11-16",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-11-17",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-11-18",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-11-19",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-11-20",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-11-21",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-11-22",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-11-23",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-11-24",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-11-25",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-11-26",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-11-27",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-11-28",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-11-29",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-11-30",
-    farmacia: "4 SOLES",
-  },
-  // DICIEMBRE 2025
-  {
-    fecha: "2025-12-01",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-12-02",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-12-03",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-12-04",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-12-05",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-12-06",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-12-07",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-12-08",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-12-09",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-12-10",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-12-11",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-12-12",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-12-13",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-12-14",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-12-15",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2025-12-16",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-12-17",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-12-18",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-12-19",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-12-20",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-12-21",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-12-22",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2025-12-23",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2025-12-24",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2025-12-25",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2025-12-26",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2025-12-27",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2025-12-28",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2025-12-29",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2025-12-30",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2025-12-31",
-    farmacia: "DEL ESTE",
-  },
-  // ENERO 2026
-  {
-    fecha: "2026-01-01",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2026-01-02",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2026-01-03",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2026-01-04",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2026-01-05",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2026-01-06",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2026-01-07",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2026-01-08",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2026-01-09",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2026-01-10",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2026-01-11",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2026-01-12",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2026-01-13",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2026-01-14",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2026-01-15",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2026-01-16",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2026-01-17",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2026-01-18",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2026-01-19",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2026-01-20",
-    farmacia: "DEL ESTE II",
-  },
-  {
-    fecha: "2026-01-21",
-    farmacia: "ITATI",
-  },
-  {
-    fecha: "2026-01-22",
-    farmacia: "FAR MAR",
-  },
-  {
-    fecha: "2026-01-23",
-    farmacia: "FARMA CAS",
-  },
-  {
-    fecha: "2026-01-24",
-    farmacia: "DEL ESTE",
-  },
-  {
-    fecha: "2026-01-25",
-    farmacia: "LOS AMIGOS",
-  },
-  {
-    fecha: "2026-01-26",
-    farmacia: "SOLIDARIA",
-  },
-  {
-    fecha: "2026-01-27",
-    farmacia: "CENTRO",
-  },
-  {
-    fecha: "2026-01-28",
-    farmacia: "CORRIENTES",
-  },
-  {
-    fecha: "2026-01-29",
-    farmacia: "4 SOLES",
-  },
-  {
-    fecha: "2026-01-30",
-    farmacia: "DEL ESTE II",
-  },
-  {
     fecha: "2026-01-31",
     farmacia: "ITATI",
   },
+ {
+  fecha: "2026-02-01",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-02-02",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-02-03",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-02-04",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-02-05",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-02-06",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-02-07",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-02-08",
+  farmacia: "4 SOLES",
+},
+{
+  fecha: "2026-02-09",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-02-10",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-02-11",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-02-12",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-02-13",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-02-14",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-02-15",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-02-16",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-02-17",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-02-18",
+  farmacia: "4 SOLES",
+},
+{
+  fecha: "2026-02-19",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-02-20",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-02-21",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-02-22",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-02-23",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-02-24",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-02-25",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-02-26",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-02-27",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-02-28",
+  farmacia: "4 SOLES",
+},
+
   
 ];
