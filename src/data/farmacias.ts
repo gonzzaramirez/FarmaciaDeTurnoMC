973f4dd359f1aafa77186b57de2d9e6955575f69
Gonzalo Ramirez <131042201+gonzzaramirez@users.noreply.github.com>
2026-05-31 00:16:27 -0300

    jun/jul

--- src/data/farmacias.ts
+++ src/data/farmacias.ts
@@ -1,487 +1,374 @@
 export const turnosFarmacias = [
-  {
-    fecha: "2026-01-31",
-    farmacia: "ITATI",
-  },
- {
-  fecha: "2026-02-01",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-02-02",
-  farmacia: "FARMA CAS",
-},
-{
-  fecha: "2026-02-03",
-  farmacia: "DEL ESTE",
-},
-{
-  fecha: "2026-02-04",
-  farmacia: "LOS AMIGOS",
-},
-{
-  fecha: "2026-02-05",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-02-06",
-  farmacia: "CENTRO",
-},
-{
-  fecha: "2026-02-07",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-02-08",
-  farmacia: "4 SOLES",
-},
-{
-  fecha: "2026-02-09",
-  farmacia: "DEL ESTE II",
-},
-{
-  fecha: "2026-02-10",
-  farmacia: "ITATI",
-},
-{
-  fecha: "2026-02-11",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-02-12",
-  farmacia: "FARMA CAS",
-},
-{
-  fecha: "2026-02-13",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-02-14",
-  farmacia: "LOS AMIGOS",
-},
-{
-  fecha: "2026-02-15",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-02-16",
-  farmacia: "CENTRO",
-},
-{
-  fecha: "2026-02-17",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-02-18",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-02-19",
-  farmacia: "DEL ESTE II",
-},
-{
-  fecha: "2026-02-20",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-02-21",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-02-22",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-02-23",
-  farmacia: "DEL ESTE",
-},
-{
-  fecha: "2026-02-24",
-  farmacia: "LOS AMIGOS",
-},
-{
-  fecha: "2026-02-25",
-  farmacia: "4 SOLES",
-},
-{
-  fecha: "2026-02-26",
-  farmacia: "CENTRO",
-},
-{
-  fecha: "2026-02-27",
-  farmacia: "ITATI",
-},
-{
-  fecha: "2026-02-28",
-  farmacia: "4 SOLES",
-},
-{
-  fecha: "2026-03-01",
-  farmacia: "DEL ESTE 2"
-},
-{
-  fecha: "2026-03-02",
-  farmacia: "ITATI",
-},
-{
-  fecha: "2026-03-03",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-03-04",
-  farmacia: "FARMA CAS",
-},
-{
-  fecha: "2026-03-05",
-  farmacia: "DEL ESTE",
-},
-{
-  fecha: "2026-03-06",
-  farmacia: "LOS AMIGOS",
-},
-{
-  fecha: "2026-03-07",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-03-08",
-  farmacia: "CENTRO",
-},
-{
-  fecha: "2026-03-09",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-03-10",
-  farmacia: "4 SOLES",
-},
-{
-  fecha: "2026-03-11",
-  farmacia: "DEL ESTE II",
-},
-{
-  fecha: "2026-03-12",
-  farmacia: "ITATI",
-},
-{
-  fecha: "2026-03-13",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-03-14",
-  farmacia: "FARMA CAS",
-},
-{
-  fecha: "2026-03-15",
-  farmacia: "DEL ESTE",
-},
-{
-  fecha: "2026-03-16",
-  farmacia: "LOS AMIGOS",
-},
-{
-  fecha: "2026-03-17",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-03-18",
-  farmacia: "CENTRO",
-},
-{
-  fecha: "2026-03-19",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-03-20",
-  farmacia: "DEL ESTE II",
-},
-{
-  fecha: "2026-03-21",
-  farmacia: "ITATI",
-},
-{
-  fecha: "2026-03-22",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-03-23",
-  farmacia: "FARMA CAS",
-},
-{
-  fecha: "2026-03-24",
-  farmacia: "DEL ESTE",
-},
-{
-  fecha: "2026-03-25",
-  farmacia: "LOS AMIGOS",
-},
-{
-  fecha: "2026-03-26",
-  farmacia: "SOLIDARIA",
-},
-{
-  fecha: "2026-03-27",
-  farmacia: "CENTRO",
-},
-{
-  fecha: "2026-03-28",
-  farmacia: "CORRIENTES",
-},
-{
-  fecha: "2026-03-29",
-  farmacia: "DEL ESTE II",
-},
-{
-  fecha: "2026-03-30",
-  farmacia: "ITATI",
-},
-{
-  fecha: "2026-03-31",
-  farmacia: "FAR MAR",
-},
-{
-  fecha: "2026-04-01",
-  farmacia: "FARMA CAS"
-},
-{
-  fecha: "2026-04-02",
-  farmacia: "DEL ESTE"
-},
-{
-  fecha: "2026-04-03",
-  farmacia: "LOS AMIGOS"
-},
-{
-  fecha: "2026-04-04",
-  farmacia: "SOLIDARIA"
-},
-{
-  fecha: "2026-04-05",
-  farmacia: "CENTRO"
-},
-{
-  fecha: "2026-04-06",
-  farmacia: "CORRIENTES"
-},
-{
-  fecha: "2026-04-07",
-  farmacia: "DEL ESTE II"
-},
-{
-  fecha: "2026-04-08",
-  farmacia: "ITATI"
-},
-{
-  fecha: "2026-04-09",
-  farmacia: "FAR MAR"
-},
-{
-  fecha: "2026-04-10",
-  farmacia: "FARMA CAS"
-},
-{
-  fecha: "2026-04-11",
-  farmacia: "DEL ESTE"
-},
-{
-  fecha: "2026-04-12",
-  farmacia: "LOS AMIGOS"
-},
-{
-  fecha: "2026-04-13",
-  farmacia: "SOLIDARIA"
-},
-{
-  fecha: "2026-04-14",
-  farmacia: "CENTRO"
-},
-{
-  fecha: "2026-04-15",
-  farmacia: "CORRIENTES"
-},
-{
-  fecha: "2026-04-16",
-  farmacia: "DEL ESTE II"
-},
-{
-  fecha: "2026-04-17",
-  farmacia: "ITATI"
-},
-{
-  fecha: "2026-04-18",
-  farmacia: "FAR MAR"
-},
-{
-  fecha: "2026-04-19",
-  farmacia: "FARMA CAS"
-},
-{
-  fecha: "2026-04-20",
-  farmacia: "DEL ESTE"
-},
-{
-  fecha: "2026-04-21",
-  farmacia: "LOS AMIGOS"
-},
-{
-  fecha: "2026-04-22",
-  farmacia: "SOLIDARIA"
-},
-{
-  fecha: "2026-04-23",
-  farmacia: "CENTRO"
-},
-{
-  fecha: "2026-04-24",
-  farmacia: "CORRIENTES"
-},
-{
-  fecha: "2026-04-25",
-  farmacia: "DEL ESTE II"
-},
-{
-  fecha: "2026-04-26",
-  farmacia: "ITATI"
-},
-{
-  fecha: "2026-04-27",
-  farmacia: "FAR MAR"
-},
-{
-  fecha: "2026-04-28",
-  farmacia: "FARMA CAS"
-},
-{
-  fecha: "2026-04-29",
-  farmacia: "DEL ESTE"
-},
-{
-  fecha: "2026-04-30",
-  farmacia: "LOS AMIGOS"
-},
 {
   fecha: "2026-05-01",
-  farmacia: "SOLIDARIA"
+  farmacia: "SOLIDARIA",
 },
 {
   fecha: "2026-05-02",
-  farmacia: "CENTRO"
+  farmacia: "CENTRO",
 },
 {
   fecha: "2026-05-03",
-  farmacia: "CORRIENTES"
+  farmacia: "CORRIENTES",
 },
 {
   fecha: "2026-05-04",
-  farmacia: "DEL ESTE II"
+  farmacia: "DEL ESTE II",
 },
 {
   fecha: "2026-05-05",
-  farmacia: "ITATI"
+  farmacia: "ITATI",
 },
 {
   fecha: "2026-05-06",
-  farmacia: "FAR MAR"
+  farmacia: "FAR MAR",
 },
 {
   fecha: "2026-05-07",
-  farmacia: "FARMA CAS"
+  farmacia: "FARMA CAS",
 },
 {
   fecha: "2026-05-08",
-  farmacia: "DEL ESTE"
+  farmacia: "DEL ESTE",
 },
 {
   fecha: "2026-05-09",
-  farmacia: "LOS AMIGOS"
+  farmacia: "LOS AMIGOS",
 },
 {
   fecha: "2026-05-10",
-  farmacia: "SOLIDARIA"
+  farmacia: "SOLIDARIA",
 },
 {
   fecha: "2026-05-11",
-  farmacia: "CENTRO"
+  farmacia: "CENTRO",
 },
 {
   fecha: "2026-05-12",
-  farmacia: "CORRIENTES"
+  farmacia: "CORRIENTES",
 },
 {
   fecha: "2026-05-13",
-  farmacia: "DEL ESTE II"
+  farmacia: "DEL ESTE II",
 },
 {
   fecha: "2026-05-14",
-  farmacia: "ITATI"
+  farmacia: "ITATI",
 },
 {
   fecha: "2026-05-15",
-  farmacia: "FAR MAR"
+  farmacia: "FAR MAR",
 },
 {
   fecha: "2026-05-16",
-  farmacia: "FARMA CAS"
+  farmacia: "FARMA CAS",
 },
 {
   fecha: "2026-05-17",
-  farmacia: "DEL ESTE"
+  farmacia: "DEL ESTE",
 },
 {
   fecha: "2026-05-18",
-  farmacia: "LOS AMIGOS"
+  farmacia: "LOS AMIGOS",
 },
 {
   fecha: "2026-05-19",
-  farmacia: "SOLIDARIA"
+  farmacia: "SOLIDARIA",
 },
 {
   fecha: "2026-05-20",
-  farmacia: "CENTRO"
+  farmacia: "CENTRO",
 },
 {
   fecha: "2026-05-21",
-  farmacia: "CORRIENTES"
+  farmacia: "CORRIENTES",
 },
 {
   fecha: "2026-05-22",
-  farmacia: "DEL ESTE II"
+  farmacia: "DEL ESTE II",
 },
 {
   fecha: "2026-05-23",
-  farmacia: "ITATI"
+  farmacia: "ITATI",
 },
 {
   fecha: "2026-05-24",
-  farmacia: "FAR MAR"
+  farmacia: "FAR MAR",
 },
 {
   fecha: "2026-05-25",
-  farmacia: "FARMA CAS"
+  farmacia: "FARMA CAS",
 },
 {
   fecha: "2026-05-26",
-  farmacia: "DEL ESTE"
+  farmacia: "DEL ESTE",
 },
 {
   fecha: "2026-05-27",
-  farmacia: "LOS AMIGOS"
+  farmacia: "LOS AMIGOS",
 },
 {
   fecha: "2026-05-28",
-  farmacia: "SOLIDARIA"
+  farmacia: "SOLIDARIA",
 },
 {
   fecha: "2026-05-29",
-  farmacia: "CENTRO"
+  farmacia: "CENTRO",
 },
 {
   fecha: "2026-05-30",
-  farmacia: "CORRIENTES"
+  farmacia: "CORRIENTES",
 },
 {
   fecha: "2026-05-31",
-  farmacia: "DEL ESTE II"
+  farmacia: "DEL ESTE II",
+},
+
+// JUNIO 2026
+{
+  fecha: "2026-06-01",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-06-02",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-06-03",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-06-04",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-06-05",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-06-06",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-06-07",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-06-08",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-06-09",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-06-10",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-06-11",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-06-12",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-06-13",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-06-14",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-06-15",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-06-16",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-06-17",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-06-18",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-06-19",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-06-20",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-06-21",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-06-22",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-06-23",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-06-24",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-06-25",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-06-26",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-06-27",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-06-28",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-06-29",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-06-30",
+  farmacia: "FARMA CAS",
+},
+
+// JULIO 2026
+{
+  fecha: "2026-07-01",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-07-02",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-07-03",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-07-04",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-07-05",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-07-06",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-07-07",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-07-08",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-07-09",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-07-10",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-07-11",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-07-12",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-07-13",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-07-14",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-07-15",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-07-16",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-07-17",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-07-18",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-07-19",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-07-20",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-07-21",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-07-22",
+  farmacia: "CENTRO",
+},
+{
+  fecha: "2026-07-23",
+  farmacia: "CORRIENTES",
+},
+{
+  fecha: "2026-07-24",
+  farmacia: "DEL ESTE II",
+},
+{
+  fecha: "2026-07-25",
+  farmacia: "ITATI",
+},
+{
+  fecha: "2026-07-26",
+  farmacia: "FAR MAR",
+},
+{
+  fecha: "2026-07-27",
+  farmacia: "FARMA CAS",
+},
+{
+  fecha: "2026-07-28",
+  farmacia: "DEL ESTE",
+},
+{
+  fecha: "2026-07-29",
+  farmacia: "LOS AMIGOS",
+},
+{
+  fecha: "2026-07-30",
+  farmacia: "SOLIDARIA",
+},
+{
+  fecha: "2026-07-31",
+  farmacia: "CENTRO",
 }
-  
 ];
